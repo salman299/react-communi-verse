fa387fa9a74f4a0576d287e9ac799000cdd3bbb8
Sahil <sahilkhowaja11@gmail.com>
2024-07-17 03:53:45 +0500

    feat: update logo

--- src/views/pages/authentication/Login.js
+++ src/views/pages/authentication/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
             <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
               <AuthCardWrapper>
                 <Grid container spacing={2} alignItems="center" justifyContent="center">
-                  <Grid item sx={{ mb: 3 }}>
+                  <Grid item sx={{ mb: 1 }}>
                     <Link to="#">
                       <AuthLogo />
                     </Link>
